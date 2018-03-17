20dd1d44479d08c8ed4fb112c362ec0bd5c9a431
MarceloGNunez <marcelo.nunez.zcfi@gmail.com>
2018-03-17 12:39:29 -0300

    Exemplo QuerySelectorAll

---
+++ Exemplo QuerySelectorAll/Classes.js
@@ -0,0 +1,3 @@
+let variavel = document.querySelectorAll("td.area");
+document.getElementById("Sping").innerHTML = variavel[0].innerHTML + ". " + variavel[1].innerHTML
+ + ". " + variavel[2].innerHTML + ". " + variavel[3].innerHTML + ". " + variavel[4].innerHTML + ". " + variavel[5].innerHTML;
