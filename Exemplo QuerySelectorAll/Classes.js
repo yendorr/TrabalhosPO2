2b5b90c77e049b915b3e2652961211bcda625d50
MarceloGNunez <marcelo.nunez.zcfi@gmail.com>
2018-04-08 22:25:30 -0300

    Arquivos antigos removidos

--- Exemplo QuerySelectorAll/Classes.js
+++
@@ -1,3 +0,0 @@
-let variavel = document.querySelectorAll("td.area");
-document.getElementById("Sping").innerHTML = variavel[0].innerHTML + ". " + variavel[1].innerHTML
- + ". " + variavel[2].innerHTML + ". " + variavel[3].innerHTML + ". " + variavel[4].innerHTML + ". " + variavel[5].innerHTML;
